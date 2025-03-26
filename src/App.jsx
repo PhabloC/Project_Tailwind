5eb33f4ca285e37b8c1c5ba5685bbf93ca996e42
Phablo Carvalho <phablocarvalho14@hotmail.com>
2025-03-26 16:27:00 -0300

    adiciona componente Header com Avatar e SearchForm, e inclui react-icons como dependência

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="h-screen bg-zinc-100 dark:bg-zinc-800 text-zinc-950 dark:text-zinc-50 grid place-content-center">
-      <h1 className="text-3xl text-center">Ready to start with Tailwind</h1>
-      <button
-        className="block w-fit mx-auto my-8 px-4 py-2 rounded-lg border-transparent hover:border-cyan-500 border-2 transition-colors"
-        onClick={() => setCount(count + 1)}
-      >
-        count is {count}
-      </button>
-    </div>
+    <>
+      <Header />
+      <div className="h-screen bg-zinc-100 dark:bg-zinc-800 text-zinc-950 dark:text-zinc-50 grid place-content-center">
+        <h1 className="text-3xl text-center">Ready to start with Tailwind</h1>
+        <button
+          className="block w-fit mx-auto my-8 px-4 py-2 rounded-lg border-transparent hover:border-cyan-500 border-2 transition-colors"
+          onClick={() => setCount(count + 1)}
+        >
+          count is {count}
+        </button>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <span className="text-2xl xl:text-3xl font-bold uppercase text-zinc-900 dark:text-zinc-50">
+      logo
+    </span>
+  );
+}
